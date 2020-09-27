4f49d82e290ba950766d8ad9ee76b2a19b1174b5
Artemiy <artemiy.dmitrochenko@gmail.com>
2020-09-27 09:15:40 +0500

    Добавляет разметку

--- src/index.js
+++ src/index.js
@@ -1,15 +1,14 @@
-import './js/common';
-import './assets/css/main.css';
+// import './js/common';
 import './assets/scss/main.scss';
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Example from './components/example/example.jsx';
-
-const init = () => {
-  ReactDOM.render(
-      <Example/>,
-      document.querySelector(`#root`)
-  );
-};
-
-init();
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import Example from './components/example/example.jsx';
+//
+// const init = () => {
+//   ReactDOM.render(
+//       <Example/>,
+//       document.querySelector(`#root`)
+//   );
+// };
+//
+// init();
